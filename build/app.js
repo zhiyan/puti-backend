53c772df5e6687377a6998daca0559216304b477
zhiyan.wang <zhiyan1987@gmail.com>
2015-08-26 15:25:43 +0800

    icon

--- build/app.js
+++ build/app.js
@@ -168,23 +168,6 @@ angular.module("backend")
     });
 
 })();
-(function(){
-  'use strict';
-
-
-  angular.module('view-location',['ngRoute'])
-    .config(function ($routeProvider) {
-      $routeProvider
-        .when('/location', {
-          templateUrl: 'location/location.html',
-          controller: 'LocationCtrl'
-        });
-    })
-    .controller('LocationCtrl', function ($scope,$rootScope) {
-      $rootScope.nav = "location";
-    });
-
-})();
 /**
  * mOxie - multi-runtime File API & XMLHttpRequest L2 Polyfill
  * v1.2.0
@@ -1498,6 +1481,23 @@ FileProgress.prototype.appear = function() {
   'use strict';
 
 
+  angular.module('view-location',['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/location', {
+          templateUrl: 'location/location.html',
+          controller: 'LocationCtrl'
+        });
+    })
+    .controller('LocationCtrl', function ($scope,$rootScope) {
+      $rootScope.nav = "location";
+    });
+
+})();
+(function(){
+  'use strict';
+
+
   angular.module('view-login',['ngRoute'])
     .config(function ($routeProvider) {
       $routeProvider
@@ -1562,91 +1562,6 @@ FileProgress.prototype.appear = function() {
     });
 
 })();
-(function() {
-    'use strict';
-
-
-    angular.module('view-news', ['ngRoute'])
-        .config(function($routeProvider) {
-            $routeProvider
-                .when('/news', {
-                    templateUrl: 'news/news.html',
-                    controller: 'NewsCtrl'
-                })
-                .when('/news/add', {
-                    templateUrl: 'news/newsAdd.html',
-                    controller: 'NewsAddCtrl'
-                })
-                .when('/news/edit/:id', {
-                    templateUrl: 'news/newsAdd.html',
-                    controller: 'NewsAddCtrl'
-                });
-        })
-        .controller('NewsCtrl', function($scope, $rootScope,$http) {
-            var URL_LIST = "/data/product.json";
-
-            $rootScope.nav = "news";
-            
-            $http.get(URL_LIST)
-                .success(function(res) {
-                    if (res.status) {
-                        $scope.list = res.data || [];
-                    }
-                })
-        })
-        .controller('NewsAddCtrl', function($scope, $rootScope, $http,$routeParams,$location,$vars) {
-
-            var URL_UPLOAD = "/data/product.json",
-                URL_GETDATA = "/data/productdetail.json";
-
-            // ckeditor
-            var editor = CKEDITOR.replace('editor',{language : 'zh-cn'});
-
-            $scope.param = {
-                "id" : $routeParams.id || "",
-                "title" : "",
-                "type" : $vars.types[0].id + "",
-                "desc" : "",
-                "body" : "",
-                "url" : ""
-            }
-
-            $scope.types = $vars.types;
-
-            if( $scope.param.id ){
-                $http.get(URL_GETDATA,{params:{id:$scope.param.id}})
-                    .success(function(res){
-                        if(res.status){
-                            $scope.param.title = res.data.title;
-                            $scope.param.desc = res.data.desc;
-                            $scope.param.type = res.data.type + "";
-                        }
-                    })
-            }
-
-            $rootScope.nav = "newsAdd";
-
-            $scope.submit = function() {
-
-                // body
-                $scope.param.body = editor.getData();
-
-                if ($scope.form.$valid) {
-                    $http.post(URL_UPLOAD,$scope.param)
-                    .success(function(res){
-                        if( res.status ){
-                            $scope.alert("提交成功");
-                            $location.path("/news")
-                        }else{  
-                            $scope.alert(res.msg);
-                        }
-                    })
-                }
-            }
-        });
-
-})();
-
 (function(){
   'use strict';
 
@@ -1760,6 +1675,91 @@ FileProgress.prototype.appear = function() {
       });
 
 })();
+(function() {
+    'use strict';
+
+
+    angular.module('view-news', ['ngRoute'])
+        .config(function($routeProvider) {
+            $routeProvider
+                .when('/news', {
+                    templateUrl: 'news/news.html',
+                    controller: 'NewsCtrl'
+                })
+                .when('/news/add', {
+                    templateUrl: 'news/newsAdd.html',
+                    controller: 'NewsAddCtrl'
+                })
+                .when('/news/edit/:id', {
+                    templateUrl: 'news/newsAdd.html',
+                    controller: 'NewsAddCtrl'
+                });
+        })
+        .controller('NewsCtrl', function($scope, $rootScope,$http) {
+            var URL_LIST = "/data/product.json";
+
+            $rootScope.nav = "news";
+            
+            $http.get(URL_LIST)
+                .success(function(res) {
+                    if (res.status) {
+                        $scope.list = res.data || [];
+                    }
+                })
+        })
+        .controller('NewsAddCtrl', function($scope, $rootScope, $http,$routeParams,$location,$vars) {
+
+            var URL_UPLOAD = "/data/product.json",
+                URL_GETDATA = "/data/productdetail.json";
+
+            // ckeditor
+            var editor = CKEDITOR.replace('editor',{language : 'zh-cn'});
+
+            $scope.param = {
+                "id" : $routeParams.id || "",
+                "title" : "",
+                "type" : $vars.types[0].id + "",
+                "desc" : "",
+                "body" : "",
+                "url" : ""
+            }
+
+            $scope.types = $vars.types;
+
+            if( $scope.param.id ){
+                $http.get(URL_GETDATA,{params:{id:$scope.param.id}})
+                    .success(function(res){
+                        if(res.status){
+                            $scope.param.title = res.data.title;
+                            $scope.param.desc = res.data.desc;
+                            $scope.param.type = res.data.type + "";
+                        }
+                    })
+            }
+
+            $rootScope.nav = "newsAdd";
+
+            $scope.submit = function() {
+
+                // body
+                $scope.param.body = editor.getData();
+
+                if ($scope.form.$valid) {
+                    $http.post(URL_UPLOAD,$scope.param)
+                    .success(function(res){
+                        if( res.status ){
+                            $scope.alert("提交成功");
+                            $location.path("/news")
+                        }else{  
+                            $scope.alert(res.msg);
+                        }
+                    })
+                }
+            }
+        });
+
+})();
+
 (function() {
     'use strict';
 
